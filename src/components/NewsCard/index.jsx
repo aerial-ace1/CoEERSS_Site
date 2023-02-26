cc8047dac1de2b495b5404733c15629839a9c6a4
Abhinav Pottigari <abhinav.pottigari@gmail.com>
2023-02-26 07:05:13 +0530

    feat: News Page

--- src/components/NewsCard/index.jsx
+++ src/components/NewsCard/index.jsx
@@ -5,16 +5,24 @@ import Card from 'react-bootstrap/Card';
 import { Link45deg } from 'react-bootstrap-icons';
 import styles from './style.module.css';
 
+const images = {};
+function importAll(r) {
+  r.keys().forEach((key) => {
+    images[key] = r(key);
+  });
+}
+importAll(require.context('./assets', false, /\.png/));
+
 const NewsCard = ({ newsItem }) => (
   <Card className={styles.cardCont} style={{ width: '25rem' }}>
-    <Card.Img variant="top" src={newsItem.image} />
+    <Card.Img variant="top" src={images[newsItem.image]} />
     <Card.Body>
       <Card.Text>
         {newsItem.text}
       </Card.Text>
       <section className={styles.links}>
         {newsItem.links.map((link) => (
-          <a href={link} target="_blank" rel="noreferrer">
+          <a href={link} key={link} target="_blank" rel="noreferrer">
             <Button variant="dark"><Link45deg /></Button>
           </a>
         ))}
